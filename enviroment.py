de652a0fe8ff0fb0e48559ef571e26e772976136
Yogurt_ <167357081+HugoCreate@users.noreply.github.com>
2025-06-04 14:42:41 -0300

    Update enviroment.py | Connection check system
    Checks for a specific access token then sends one back to the "gate.py"

--- enviroment.py
+++ enviroment.py
@@ -1,10 +1,12 @@
 import socket as sk
-import threading
 import os
 import subprocess as sub
 import time
 
 main_skt = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+ENV_HOST = "localhost"
+ACCESS_PORT = 12555
+CONFIRMATION_TOKEN = r"ACCESS_GIVEN{12341234}"
 
 for i in range(0, 5):
   print("Starting shell enviroment... \\")
@@ -16,3 +18,16 @@ for i in range(0, 5):
   print("Starting shell enviroment... /")
   time.sleep(1)
   os.system("cls")
+
+  main_skt.bind((ENV_HOST, ACCESS_PORT))
+  main_skt.listen()
+  conn, addr = main_skt.accept()
+
+  with conn:
+    entry_token = main_skt.recv(1024)
+    if entry_token.decode() == r"Acces_Token_Test{12341234}":
+        print(f"Connection stablished! With host: {addr}")
+        main_skt.close()
+        skt2 = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+        skt2.connect(addr)
+        skt2.sendall(CONFIRMATION_TOKEN.encode())
